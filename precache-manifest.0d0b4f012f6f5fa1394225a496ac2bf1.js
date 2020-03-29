18eb28b40fdfc39422133f40db6187ad2ab5fa87
minecodebot <minecode.geral@gmail.com>
2020-03-29 15:21:04 +0000

    deploy: c9aaacfcf2bfca6cf1625d31e00f44eaa2084f69

---
+++ precache-manifest.0d0b4f012f6f5fa1394225a496ac2bf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a14c09ae8ef2f4eef13fb9d72d27df64",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "1a6f9aacfc9ef6aee633",
+    "url": "/covid19/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e051ff9186dca5829693",
+    "url": "/covid19/static/js/2.3c4d5a01.chunk.js"
+  },
+  {
+    "revision": "a39446133f0833aa16763010660e7a8d",
+    "url": "/covid19/static/js/2.3c4d5a01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a6f9aacfc9ef6aee633",
+    "url": "/covid19/static/js/main.2ab45f9e.chunk.js"
+  },
+  {
+    "revision": "2897f32d4e7477ba260b",
+    "url": "/covid19/static/js/runtime-main.a021e852.js"
+  }
+]);
